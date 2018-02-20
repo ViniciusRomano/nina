4cdcf1575a76f1d6ae725d30dc5bdd3e6fe6dad9
romano <romano@alunos.utfpr.edu.br>
2018-02-19 23:23:34 -0300

    ultimas modificações

--- js/test.js
+++ js/test.js
@@ -1,74 +1,152 @@
 // Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyB7NziL8iza-SRgQDpFV86Ud0GxGwxhM2U",
+  authDomain: "nina-20725.firebaseapp.com",
+  databaseURL: "https://nina-20725.firebaseio.com/",
+  projectId: "nina-20725",
+  storageBucket: "gs://nina-20725.appspot.com/",
+  messagingSenderId: "959133269945"
+});
 
-  var config = {
-    apiKey: "AIzaSyCYh4W6UEQrSCmx-RpSznDkxdigTPqSxNs",
-    authDomain: "fir-test-cb75a.firebaseapp.com",
-    databaseURL: "https://fir-test-cb75a.firebaseio.com",
-    projectId: "fir-test-cb75a",
-    storageBucket: "fir-test-cb75a.appspot.com",
-    messagingSenderId: "1006507312413"
-  };
+var inicialSettings = {
+  init: function () {
+    inicialSettings.loadData();
+    inicialSettings.loadButton();
+    inicialSettings.onUpdate();
+  },
+  loadButton: function () {
+    $("#submit-button").on('click', function () {
+      var date = new Date(+Date.now());
+      var year = date.getFullYear();
+      var month = date.getMonth() + 1;
+      var day = date.getDate();
+      var hour = date.getHours();
+      var min = "0" + date.getMinutes();
+      var formattedDate = day + "-" + month + "-" + year;
+      var formattedTime = hour + ":" + min.substr(-2);
+      var ra = document.getElementById("ra").value;
+      var indice = 0;
 
-  firebase.initializeApp(config);
-  var dbFirebase = firebase.database().ref();
+      var ref = firebase.database().ref("pessoas/" + ra);
+      if (ra == '') {
+        swal({
+          type: 'error',
+          title: 'RA não cadastrado!',
+          text: 'Este RA não é de um membro da Unect Jr.',
+          footer: 'Caso necessário, entre em contato com a diretoria de projetos.',
+        });
+      } else {
+        ref.once("value").then(function (snapshot) {
+          if (snapshot.exists()) {
+            var nome = snapshot.val();
+            var ref = firebase.database().ref("permanencias/" + formattedDate);
+            ref.once("value").then(function (snapshot) {
+              if (snapshot.exists()) {
+                while (snapshot.child(ra + "/" + indice + "/finalizado").val()) {
+                  indice++;
+                }
+                if (snapshot.child(ra + "/" + indice).exists()) {
+                  var inicio = snapshot.child(ra + "/" + indice + "/horainicio").val();
+                  var inicioHour = parseInt(inicio.substr(0, 2));
+                  var inicioMin = parseInt(inicio.substr(3, 5));
+                  var formattedHour = parseInt(formattedTime.substr(0, 2));
+                  var formattedMin = parseInt(formattedTime.substr(3, 5));
+                  var horasT = ("0" + (formattedHour - inicioHour)).substr(-2) + ":" + ("0" + (formattedMin - inicioMin)).substr(-2);
+                  ref.child(ra + "/" + indice).update({
+                    "horatermino": formattedTime,
+                    "horastotais": horasT,
+                    "finalizado": true
+                  });
+                  swal({
+                    position: 'center',
+                    type: 'success',
+                    title: 'Permanência fechada com sucesso!',
+                    showConfirmButton: false,
+                    timer: 2200
+                  });
 
-  function writePessoaData(){
-
+                } else {
+                  ref.child(ra + "/" + indice).set({
+                    "nome": nome,
+                    "horainicio": formattedTime,
+                    "horatermino": null,
+                    "horastotais": null,
+                    "finalizado": false
+                  });
+                  swal({
+                    position: 'center',
+                    type: 'success',
+                    title: 'Permanência aberta com sucesso!',
+                    showConfirmButton: false,
+                    timer: 2200
+                  });
+                }
+              } else {
+                ref.child(ra + "/0").set({
+                  "nome": nome,
+                  "horainicio": formattedTime,
+                  "horatermino": null,
+                  "horastotais": null,
+                  "finalizado": false
+                });
+                swal({
+                  position: 'center',
+                  type: 'success',
+                  title: 'Entrada na permanência realizada com sucesso!',
+                  showConfirmButton: false,
+                  timer: 2200
+                });
+              }
+            }).
+            catch(function (error) {
+              swal(
+                'Está conectado à internet?',
+                'Tente reconectar e repita  o procedimento!',
+                'question'
+              )
+            });
+          } else {
+            swal({
+              type: 'error',
+              title: 'RA não cadastrado!',
+              text: 'Este RA não é de um membro da Unect Jr.',
+              footer: 'Caso necessário, entre em contato com a diretoria de projetos.',
+            });
+          }
+        });
+      }
+    });
+  },
+  loadData: function () {
     var date = new Date(+Date.now());
     var year = date.getFullYear();
     var month = date.getMonth() + 1;
     var day = date.getDate();
-    var hour = date.getHours();
-    var min = "0"+date.getMinutes();
     var formattedDate = day + "-" + month + "-" + year;
-    var formattedTime = hour + ":" + min.substr(-2);
-    var ra = document.getElementById("ra").value;
-    var indice = 0;
-
-    var ref = firebase.database().ref("pessoas/"+ra);
-    ref.once("value").then(function(snapshot){
-      if(snapshot.exists()){
-        var nome = snapshot.child("nome").val();
-        var ref = firebase.database().ref("permanencias/"+formattedDate);
-        ref.once("value").then(function(snapshot){
-          if(snapshot.exists()){
-            while(snapshot.child(ra+"/"+indice+"/finalizado").val()){
-              indice++;
-            }
-            if(snapshot.child(ra+"/"+indice).exists()){
-              var inicio = snapshot.child(ra+"/"+indice+"/horainicio").val();
-              var inicioHour = parseInt(inicio.substr(0,2));
-              var inicioMin = parseInt(inicio.substr(3,5));
-              var formattedHour = parseInt(formattedTime.substr(0,2));
-              var formattedMin = parseInt(formattedTime.substr(3,5));
-              var horasT = ("0"+(formattedHour-inicioHour)).substr(-2)+":"+("0"+(formattedMin-inicioMin)).substr(-2);
-              ref.child(ra+"/"+indice).update({
-                "horatermino" : formattedTime,
-                "horastotais" : horasT,
-                "finalizado" : true
-              });
-
-            }else{
-              ref.child(ra+"/"+indice).set({
-                  "nome" : nome,
-                  "horainicio" : formattedTime,
-                  "horatermino" : null,
-                  "horastotais" : null,
-                  "finalizado" : false
-              });
-            }
-          }else{
-            ref.child(ra+"/0").set({
-              "nome" : nome,
-              "horainicio" : formattedTime,
-              "horatermino" : null,
-              "horastotais" : null,
-              "finalizado" : false
-            });
+    var ref = firebase.database().ref("permanencias/" + formattedDate);
+    ref.once("value").then(function (snapshot) {
+      console.log(snapshot.numChildren())
+      var trHTML = '';
+      jQuery.each(snapshot.val(), function (ra, permanencias) {
+        jQuery.each(permanencias, function (i, perm) {
+          if (perm.finalizado) {
+            trHTML += '<tr><td>' + perm.nome + '</td><td>' + perm.horainicio + '</td><td>' + perm.horatermino + '</td><td>' + perm.horastotais + '</td><td><img class="finalizado" id="final-tabela" src="../img/true.png" alt=""></td></td></tr>';
+          } else {
+            trHTML += '<tr><td>' + perm.nome + '</td><td>' + perm.horainicio + '</td><td>-</td><td>-</td><td><img class="finalizado" id="final-tabela" src="../img/false.png" alt=""></td></tr>';
           }
         });
-      }else{
-        alert("Usuário não cadastrado !");
-      }
+      });
+      $('#table-body').empty().append(trHTML);
+    });
+  },
+  onUpdate: function () {
+    var ref = firebase.database().ref("permanencias");
+    ref.on("child_changed", function (snapshot) {
+      inicialSettings.loadData();
     });
   }
+};
+
+$(document).ready(function () {
+  inicialSettings.init();
+});
